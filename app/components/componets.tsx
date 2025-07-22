eee459902f4dcaa28a80c472257ab1a3e0fd05dc
viprobert <vip.robert14@gmail.com>
2025-07-22 18:36:39 +0700

    Image draggable false

--- app/components/componets.tsx
+++ app/components/componets.tsx
@@ -19,6 +19,7 @@ export default function RonaldinhoPage() {
               width={219} 
               height={70} 
               className="h-auto object-contain"
+              draggable = "false"
               priority
             />
             <Image
@@ -27,6 +28,7 @@ export default function RonaldinhoPage() {
               width={63}
               height={40}
               className="mx-5 h-auto object-contain"
+              draggable = "false"
               priority
             />
             <Image
@@ -34,7 +36,8 @@ export default function RonaldinhoPage() {
               alt="Ronaldinho Logo"
               width={247} 
               height={75} 
-              className="h-auto object-contain" 
+              className="h-auto object-contain"
+              draggable = "false" 
               priority
             />
           </div>
@@ -44,6 +47,7 @@ export default function RonaldinhoPage() {
               alt="MB66 Logo"
               width={50} 
               height={18} 
+              draggable = "false"
               className="h-auto object-contain"
               priority
             />
@@ -52,6 +56,7 @@ export default function RonaldinhoPage() {
               alt="Middle Icons"
               width={14}
               height={12}
+              draggable = "false"
               className="mx-2 h-auto object-contain"
               priority
             />
@@ -60,6 +65,7 @@ export default function RonaldinhoPage() {
               alt="Ronaldinho Logo"
               width={55} 
               height={19} 
+              draggable = "false"
               className="h-auto object-contain" 
               priority
             />
@@ -72,6 +78,7 @@ export default function RonaldinhoPage() {
           alt="MB66"
           width={1920}
           height={111}
+          draggable = "false"
           className="hidden md:block w-full h-auto object-cover"
           priority
           >
@@ -82,6 +89,7 @@ export default function RonaldinhoPage() {
             alt="MB66"
             width={430}
             height={25}
+            draggable = "false"
             className="block md:hidden w-full h-auto object-cover"
             priority
           />
@@ -97,6 +105,7 @@ export default function RonaldinhoPage() {
               alt="หน้าหลัก MB66"
               width={422}
               height={58}
+              draggable = "false"
               className="hidden md:block w-[422px] md:w-[400px] hover:scale-105 transition-transform duration-300"
             />
             <Image
@@ -104,6 +113,7 @@ export default function RonaldinhoPage() {
               alt="หน้าหลัก MB66 (Mobile)" 
               width={141}
               height={20}
+              draggable = "false"
               className="block md:hidden w-[200px] hover:scale-105 transition-transform duration-300" 
             />
           </Link>
@@ -136,7 +146,7 @@ export default function RonaldinhoPage() {
         <div className="w-full max-w-6xl mx-auto mb-0">
         {/* Desktop version */}
           <div className="hidden md:block">
-            <h2 className="text-[35px] md:h-[65px] md:text-[30px] text-white font-bold md:mb-8 bg-[url('/img/content1_title.png')] bg-cover bg-center px-8 text-center">
+            <h2 className="text-[35px] md:h-[65px] md:text-[30px] text-white font-bold md:mb-8 bg-[url('/img/content1_title.png')] bg-cover bg-center px-8 text-center" >
           RONALDINHO และความร่วมมือในฐานะแบรนด์แอมบาสเดอร์ของ MB66
            </h2>
 
@@ -144,35 +154,37 @@ export default function RonaldinhoPage() {
             <div className="w-1/2 flex flex-col"> 
               <div className="text-black text-base md:text-lg md:text-[16px]"> 
                 <Image
-                src="/img/content1_text.png"
-                alt="content1"
-                width={648} 
-                height={386}
-                className="self-start mt-auto"
-              />
+                  src="/img/content1_text.png"
+                  alt="content1"
+                  width={648} 
+                  height={386}
+                  draggable = "false"
+                  className="self-start mt-auto"
+                />
               </div>
-            <Image
-              src="/img/content1_sign.png"
-              alt="sign"
-              width={375} 
-              height={280}
-              max-height={300} 
-              className="self-start mt-auto" 
-            />
-      </div>
+              <Image
+                src="/img/content1_sign.png"
+                alt="sign"
+                width={375} 
+                height={280}
+                max-height={300} 
+                draggable = "false"
+                className="self-start mt-auto" 
+              />
+            </div>
 
-      <div className="w-1/2"> 
-        <Image
-          src="/img/content1.webp"
-          alt="group1 desktop"
-          width={617} 
-          height={659} 
-          className="w-full h-auto object-contain"
-        />
-      </div>
-    </div>
+            <div className="w-1/2"> 
+              <Image
+                src="/img/content1.webp"
+                alt="group1 desktop"
+                width={617} 
+                height={659} 
+                draggable = "false"
+                className="w-full h-auto object-contain"
+              />
+            </div>
           </div>
-
+          </div>
           {/* Mobile version */}
           <div className="block md:hidden">
             <Image
@@ -180,6 +192,7 @@ export default function RonaldinhoPage() {
               alt="Content 1 Title"
               width={430}
               height={40}
+              draggable = "false"
               className="mb-4 w-full"
             />
             <Image
@@ -187,6 +200,7 @@ export default function RonaldinhoPage() {
               alt="Content 1 Group"
               width={430}
               height={250}
+              draggable = "false"
               className="w-full mb-4"
             />
             <div className="flex justify-center items-center mb-6 w-full h-[150px] px-4">
@@ -195,6 +209,7 @@ export default function RonaldinhoPage() {
                 alt="Content 1 Sign"
                 width={300}
                 height={100}
+                draggable = "false"
                 className="h-auto object-contain"
               />
             </div>
@@ -206,6 +221,7 @@ export default function RonaldinhoPage() {
                 alt="Content 1 Text1"
                 width={430}
                 height={130}
+                draggable = "false"
                 className="mb-4 w-full"
               />
               <Image
@@ -213,6 +229,7 @@ export default function RonaldinhoPage() {
                 alt="Content 1 Text2"
                 width={430}
                 height={105}
+                draggable = "false"
                 className="mb-4 w-full"
               />
               <Image
@@ -220,6 +237,7 @@ export default function RonaldinhoPage() {
                 alt="Content 1 Text3"
                 width={430}
                 height={55}
+                draggable = "false"
                 className="mb-4 w-full"
               />
               <Image
@@ -227,6 +245,7 @@ export default function RonaldinhoPage() {
                 alt="Content 1 Text4"
                 width={430}
                 height={152}
+                draggable = "false"
                 className="mb-8 w-full"
               />
             </div>
@@ -243,6 +262,7 @@ export default function RonaldinhoPage() {
               alt="Content 2 Title"
               width={1290} 
               height={100} 
+              draggable = "false"
               className="w-full mb-14" 
             />
 
@@ -253,6 +273,7 @@ export default function RonaldinhoPage() {
                   alt="Content 2 Group"
                   width={600} 
                   height={700}
+                  draggable = "false"
                   className="w-full h-auto object-contain"
                 />
               </div>
@@ -263,6 +284,7 @@ export default function RonaldinhoPage() {
                   alt="Content 2"
                   width={600} 
                   height={400} 
+                  draggable = "false"
                   className="w-full h-auto object-contain"
                 />
               </div>
@@ -274,6 +296,7 @@ export default function RonaldinhoPage() {
                   alt="Content 2 Name"
                   width={1152} 
                   height={300} 
+                  draggable = "false"
                   className="absolute bottom-0 left-0 w-full z-0" 
                   style={{ transform: 'translateY(64%)' }} 
                 />
@@ -289,15 +312,17 @@ export default function RonaldinhoPage() {
               alt="Content 2 Title Mobile"
               width={430} 
               height={50} 
+              draggable = "false"
               className="w-full mb-6"
             />
 
-            <div className="relative"> {/* Container for mobile image and overlap */}
+            <div className="relative"> 
               <Image
                 src="/img/content2_mobile.png"
                 alt="Content 2 Group mobile"
                 width={430} 
                 height={300} 
+                draggable = "false"
                 className="w-full h-auto object-contain relative z-10"
               />
               {/* Overlapping bottom image for mobile */}
@@ -306,6 +331,7 @@ export default function RonaldinhoPage() {
                 alt="Content 2 name mobile"
                 width={430} 
                 height={100} 
+                draggable = "false"
                 className="absolute bottom-0 left-0 w-full z-0" 
                 style={{ transform: 'translateY(63%)' }} 
               />
@@ -318,6 +344,7 @@ export default function RonaldinhoPage() {
                 alt="Content 2 Text1"
                 width={430} 
                 height={160} 
+                draggable = "false"
                 className="w-full mb-6"
               />
               <Image
@@ -325,6 +352,7 @@ export default function RonaldinhoPage() {
                 alt="Content 2 Text2"
                 width={430} 
                 height={80} 
+                draggable = "false"
                 className="w-full mb-4"
               />
               <Image
@@ -332,6 +360,7 @@ export default function RonaldinhoPage() {
                 alt="Content 2 Text3"
                 width={430} 
                 height={130} 
+                draggable = "false"
                 className="w-full"
               />
             </div>
@@ -346,6 +375,7 @@ export default function RonaldinhoPage() {
             alt="Ronaldinho info desktop"
             width={1290}
             height={240}
+            draggable = "false"
             className="w-full h-auto hidden md:block md:mb-6"
           />
           <Image
@@ -353,11 +383,11 @@ export default function RonaldinhoPage() {
             alt="Ronaldinho image desktop"
             width={1290}
             height={798}
+            draggable = "false"
             className="w-full h-auto hidden md:block"
           />
 
           {/* mobile*/}
-
           <div className="block md:hidden text-black text-sm mt-8">
             <h3 className="mb-5 text-[22px] font-weight-[900] font-bold">
               RONALDO DE ASSIS MOREIRA
@@ -367,6 +397,7 @@ export default function RonaldinhoPage() {
               alt="Context3 Text1"
               width={430}
               height={180}
+              draggable = "false"
               className="w-full h-auto block md:hidden mb-4"
             />
             <Image
@@ -374,6 +405,7 @@ export default function RonaldinhoPage() {
               alt="Context3 Text2"
               width={430}
               height={178}
+              draggable = "false"
               className="w-full h-auto block md:hidden mb-6"
             />
           </div>
@@ -382,6 +414,7 @@ export default function RonaldinhoPage() {
             alt="Context3 Text3"
             width={430}
             height={260}
+            draggable = "false"
             className="w-full h-auto block md:hidden"
           />
         </div>
@@ -395,6 +428,7 @@ export default function RonaldinhoPage() {
           alt="Achievement Title"
           width={1100} 
           height={67} 
+          draggable = "false"
           className="hidden md:block w-full mb-14" 
         />
         <div className="relative">
@@ -403,6 +437,7 @@ export default function RonaldinhoPage() {
             alt="Ronaldinho Honors Desktop"
             width={1080} 
             height={304} 
+            draggable = "false"
             className="hidden md:block mx-auto max-w-full h-auto rounded-lg"
           />
           <Image
@@ -410,6 +445,7 @@ export default function RonaldinhoPage() {
             alt="Ronaldinho Honors (mobile)"
             width={320} 
             height={160} 
+            draggable = "false"
             className="block md:hidden mx-auto max-w-full h-auto rounded-lg mt-6"
           />
         </div>
@@ -433,6 +469,7 @@ export default function RonaldinhoPage() {
             alt="MB66 mid banner"
             width={1920}
             height={90}
+            draggable = "false"
             className="hidden md:block w-full h-auto"
           />
         </div>
@@ -453,6 +490,7 @@ export default function RonaldinhoPage() {
             alt="Footer Logo"
             width={1920}
             height={380}
+            draggable = "false"
             className="hidden md:block w-full h-auto object-cover left-0"
             priority
           />
@@ -461,6 +499,7 @@ export default function RonaldinhoPage() {
             alt="Footer Logo (mobile)"
             width={438}
             height={136}
+            draggable = "false"
             className="block md:hidden w-full h-auto object-cover absolute"
             priority
           />
