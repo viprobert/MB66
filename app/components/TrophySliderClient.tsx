e81d0a86177dfffa1d421ab187a18b374e398e14
viprobert <vip.robert14@gmail.com>
2025-07-22 18:43:25 +0700

    trophy auto play

--- app/components/TrophySliderClient.tsx
+++ app/components/TrophySliderClient.tsx
@@ -55,7 +55,7 @@ export default function TrophySliderClient() {
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 3000,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
