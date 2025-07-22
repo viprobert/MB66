d03a2e81e97e1583da4d99d00719aa0cbdc92492
viprobert <vip.robert14@gmail.com>
2025-07-22 15:34:33 +0700

    font change

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@400;500;700&display=swap" rel="stylesheet" />
         {/* Google Analytics Script */}
         <Script
           strategy="afterInteractive"
